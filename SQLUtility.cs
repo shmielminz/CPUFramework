df8fb0bb6252d0512756c8bd6b87b8dfced66e14
Shmiel Minz <s0583223395@gmail.com>
2023-02-23 21:41:59 +0000

    Session 20

--- SQLUtility.cs
+++ SQLUtility.cs
@@ -29,15 +29,23 @@ namespace CPUFramework
 
         public static DataTable GetDataTable(SqlCommand cmd)
         {
-            Debug.Print("-------" + Environment.NewLine + cmd.CommandText);
             DataTable dt = new();
 
             using (SqlConnection conn = new(ConnectionString))
             {
                 conn.Open();
                 cmd.Connection = conn;
-                SqlDataReader dr = cmd.ExecuteReader();
-                dt.Load(dr);
+                Debug.Print(GetSQL(cmd));
+                try
+                {
+                    SqlDataReader dr = cmd.ExecuteReader();
+                    dt.Load(dr);
+                }
+                catch (SqlException ex)
+                {
+                    string msg = ParseConstraintMsg(ex.Message);
+                    throw new Exception(msg);
+                }
             }
 
             SetAllColumnsAllowNull(dt);
@@ -53,6 +61,42 @@ namespace CPUFramework
         public static void ExecuteSql(string sqlstatement)
         {
             GetDataTable(sqlstatement);
+        }
+
+        private static string ParseConstraintMsg(string msg)
+        {
+            string origmsg = msg;
+            string prefix = "ck_";
+            string msgend = "";
+            if (!msg.Contains(prefix))
+            {
+                if (msg.Contains("u_"))
+                {
+                    prefix = "u_";
+                    msgend = " must be unique.";
+                }
+                else if (msg.Contains("f_"))
+                {
+                    prefix = "f_";
+                }
+            }
+            if (msg.Contains(prefix))
+            {
+                int pos = msg.IndexOf(prefix) + prefix.Length;
+                msg = msg.Substring(pos);
+                pos = msg.IndexOf(" ") - 2;
+                if (pos == -1)
+                {
+                    msg = origmsg;
+                }
+                else
+                {
+                    msg = msg.Substring(0, pos);
+                    msg = msg.Replace("_", " ");
+                    msg = msg + msgend;
+                }
+            }
+            return msg;
         }
 
         public static int GetFirstColumnFirstRowValue(string sql)
@@ -77,6 +121,47 @@ namespace CPUFramework
             {
                 c.AllowDBNull = true;
             }
+        }
+
+        public static string GetSQL(SqlCommand cmd)
+        {
+            string val = "";
+#if DEBUG
+            StringBuilder sb = new();
+            
+            if (cmd.Connection != null)
+            {
+                sb.AppendLine($"--{cmd.Connection.DataSource}");
+                sb.AppendLine($"use {cmd.Connection.Database}");
+                sb.AppendLine("go");
+            }
+
+            if (cmd.CommandType == CommandType.StoredProcedure)
+            {
+                sb.AppendLine($"exec {cmd.CommandText}");
+                int paramcount = cmd.Parameters.Count - 1;
+                int paramnum = 0;
+                foreach (SqlParameter p in cmd.Parameters)
+                {
+                    if (p.Direction != ParameterDirection.ReturnValue)
+                    {
+                        sb.Append($"{p.ParameterName} = {(p.Value == null ? "null" : p.Value.ToString())}");
+                        if (paramnum != paramcount)
+                        {
+                            sb.AppendLine(",");
+                        }
+                    }
+                    paramnum++;
+                }
+            }
+            else
+            {
+                sb.AppendLine(cmd.CommandText);
+            }
+
+            val = sb.ToString();
+#endif
+            return val;
         }
 
         public static void DebugPrintDataTable(DataTable dt)

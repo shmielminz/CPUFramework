ff737ae94d24d19ef2feb974e47c949fe85b1747
Shmiel Minz <s0583223395@gmail.com>
2023-03-12 15:27:23 +0000

    Updated to remove usings

--- SQLUtility.cs
+++ SQLUtility.cs
@@ -1,12 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Data.SqlClient;
+﻿using System.Data.SqlClient;
 using System.Data;
-using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
 using System.Diagnostics;
-using System.Reflection.Metadata;
 
 namespace CPUFramework
 {

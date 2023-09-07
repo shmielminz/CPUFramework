5f9fb93a54ab5400ab9d3c30e52f712ba29f1782
Shmiel Minz <s0583223395@gmail.com>
2023-09-07 19:21:09 +0100

    Finished part 2 session 40

---
+++ bizObject.cs
@@ -0,0 +1,48 @@
+﻿using System.Data.SqlClient;
+using System.Data;
+
+namespace CPUFramework
+{
+    public class bizObject
+    {
+        string _tablename = ""; string _getsproc = ""; string _updatesproc = ""; string _deletesproc = "";
+        string _primarykeyname = ""; string _primarykeyparamname = "";
+        DataTable _datatable = new();
+        public bizObject(string tablename)
+        {
+            _tablename = tablename;
+            _getsproc = tablename + "Get";
+            _updatesproc = tablename + "Update";
+            _deletesproc = tablename + "Delete";
+            _primarykeyname = tablename + "Id";
+            _primarykeyparamname = "@" + _primarykeyname;
+        }
+        public DataTable Load(int primarykeyvalue)
+        {
+            DataTable dt;
+            SqlCommand cmd = SQLUtility.GetSqlCommand(_getsproc);
+            SQLUtility.SetParamValue(cmd, _primarykeyparamname, primarykeyvalue);
+            dt = SQLUtility.GetDataTable(cmd);
+            _datatable = dt;
+            return dt;
+        }
+        public void Delete(DataTable datatable)
+        {
+            int id = (int)datatable.Rows[0][_primarykeyname];
+            SqlCommand cmd = SQLUtility.GetSqlCommand(_deletesproc);
+            SQLUtility.SetParamValue(cmd, _primarykeyparamname, id);
+            SQLUtility.ExecuteSql(cmd);
+        }
+
+        public void Save(DataTable datatable)
+        {
+            if (datatable.Rows.Count == 0)
+            {
+                throw new Exception($"Cannot call {_tablename} Save method, there were no rows returned from table.");
+            }
+            DataRow r = datatable.Rows[0];
+
+            SQLUtility.SaveDataRow(r, _updatesproc);
+        }
+    }
+}
